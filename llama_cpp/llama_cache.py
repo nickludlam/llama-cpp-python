36315c2b0cea7ca88e78fdc89ed9f8f245633a5c
Nick Ludlam <nick@recoil.org>
2024-09-11 16:09:31 +0100

    Pass capacity_bytes to diskcache.Cache() as size_limit to allow caches > 1GB

--- llama_cpp/llama_cache.py
+++ llama_cpp/llama_cache.py
@@ -108,7 +108,7 @@ class LlamaDiskCache(BaseLlamaCache):
         self, cache_dir: str = ".cache/llama_cache", capacity_bytes: int = (2 << 30)
     ):
         super().__init__(capacity_bytes)
-        self.cache = diskcache.Cache(cache_dir)
+        self.cache = diskcache.Cache(cache_dir, size_limit=capacity_bytes)
 
     @property
     def cache_size(self):
